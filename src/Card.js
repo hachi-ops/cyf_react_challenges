195889d44b5747932a924c9fbff305c87a59d5bf
hachi-ops <elen20@protonmail.com>
2022-04-14 20:00:04 +0100

    images

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,23 @@
 import React from "react";
 
+const fakeData = [
+      {
+    "id": 1,
+    "company": "Photosnap",
+    "logo": "./images/photosnap.svg",
+    "new": true,
+    "featured": true,
+    "position": "Senior Frontend Developer",
+    "role": "Frontend",
+    "level": "Senior",
+    "postedAt": "1d ago",
+    "contract": "Full Time",
+    "location": "USA Only",
+    "languages": ["HTML", "CSS", "JavaScript"],
+    "tools": []
+  },
+]
+
 
 function Card() {
     return (
