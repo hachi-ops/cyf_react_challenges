107ad1d2c0ef6170bf19e0c6f7465d6b768b0668
hachi-ops <elen20@protonmail.com>
2022-04-18 15:59:15 +0100

    made components/added assests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+// import jobsData from './data';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <JobListing />
     </div>
   );
 }
 
+
+function JobListing () {
+    return (
+        <div>
+            <CompanyLogo />
+            <CompanyName />
+            <JobTitle />
+            <JobBriefDetails />
+            <JobTags />
+        </div>
+    )
+}
+
+function CompanyLogo () {
+  return (
+    <div>CompanyLogo</div>
+  )
+}
+
+function CompanyName () {
+  return (
+    <div>CompanyName</div>
+  )
+}
+
+function JobTitle () {
+  return (
+    <div>JobTitle</div>
+  )
+}
+
+function JobBriefDetails () {
+  return (
+    <div>JobBriefDetails</div>
+  )
+}
+
+function JobTags () {
+  return (
+    <div>JobTags</div>
+  )
+}
 export default App;
---
+++ src/components/JobListing.js
@@ -0,0 +1,15 @@
+// import React from "react";
+
+// function JobListing () {
+//     return (
+//         <div>
+//             <div>CompanyLogo</div>
+//             <div>CompanyName</div>
+//             <div>JobTitle</div>
+//             <div>JobBriefDetails</div>
+//             <div>JobTags</div>
+//         </div>
+//     )
+// }
+
+// import default JobListing;
