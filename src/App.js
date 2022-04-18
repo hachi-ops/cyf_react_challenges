0b4f23fe3c4a9c50dd84fb3e30189c513f85c23b
hachi-ops <elen20@protonmail.com>
2022-04-18 19:33:27 +0100

    JobListing component
    -mapped through data.js

--- src/App.js
+++ src/App.js
@@ -12,82 +12,54 @@ function App() {
 function JobListing () {
     return (
         <div>
-            <CompanyLogo />
-            <CompanyName />
-            <JobTitle />
-            <JobBriefDetails />
-            <JobTags />
+          {jobsData.map((data) => {
+            return (
+              <div>
+                <div>
+                  <img src={data.logo} alt={"logo"} style={{width: "80px"}} />
+                </div>                
+                <h1>{data.company}</h1>
+                <h2>{data.position}</h2>            
+                <div>JobBriefDetails</div>
+                <div>JobTags</div>                                         
+              </div>
+            )
+          })}
+          
         </div>
     )
 }
 
-function CompanyLogo () {
-  return (
-    <div>
-      {jobsData.map((logo)=>{
-        return (
-          <img src={logo.logo} alt={"logo"} style={{width: "80px"}} />
-        )
-      })}
-    </div>
-  )
-}
+// function JobBriefDetails () {
+//   return (
+//     <div>
+//       {jobsData.map((jobBriefDetails) => {
+//         return (
+//           <ul>
+//             <li>{jobBriefDetails.postedAt}</li>
+//             <li>{jobBriefDetails.contract}</li>
+//             <li>{jobBriefDetails.location}</li>
+//           </ul>
+//         )
+//       })}
+//     </div>
+//   )
+// }
 
-function CompanyName () {
-  return (
-    <div>
-      {jobsData.map((company) => {
-        return (
-          <h1>{company.company}</h1>
-        )
-      })}
-    </div>
-  )
-}
-
-function JobTitle () {
-  return (
-    <div>
-      {jobsData.map((position) => {
-        return (
-          <h1>{position.position}</h1>
-        )
-      })}
-  
-    </div>
-  )
-}
-
-function JobBriefDetails () {
-  return (
-    <div>
-      {jobsData.map((jobBriefDetails) => {
-        return (
-          <ul>
-            <li>{jobBriefDetails.postedAt}</li>
-            <li>{jobBriefDetails.contract}</li>
-            <li>{jobBriefDetails.location}</li>
-          </ul>
-        )
-      })}
-    </div>
-  )
-}
-
-function JobTags () {
-  return (
-    <div>
-      {jobsData.map((jobTags) => {
-        return (
-          <ul>
-            <li>{jobTags.role}</li>
-            <li>{jobTags.level}</li>
-            <li>{jobTags.languages}</li>
-            <li>{jobTags.tools}</li>
-          </ul>
-        )
-      })}
-    </div>
-  )
-}
+// function JobTags () {
+//   return (
+//     <div>
+//       {jobsData.map((jobTags) => {
+//         return (
+//           <ul>
+//             <li>{jobTags.role}</li>
+//             <li>{jobTags.level}</li>
+//             <li>{jobTags.languages}</li>
+//             <li>{jobTags.tools}</li>
+//           </ul>
+//         )
+//       })}
+//     </div>
+//   )
+// }
 export default App;
