a8b79ff3f64f737430ef7195c6dee47a7629c5f3
hachi-ops <elen20@protonmail.com>
2022-04-27 00:56:01 +0100

    some css

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ const getData = () => {
 
     return(
         <div>
-          <header className="App-header">
+          <header className="header">
             <h1>Where in the World?</h1>
             <img src={moon} alt={"moon"}/>
             <h2>Dark Mode</h2>
@@ -85,11 +85,13 @@ const getData = () => {
                  .map((country) => {
                 return (
                   <div className="card" >
-              <img className='flag' src={country.flags.svg} alt={""} style={{width:"300px"}}/>
+                <img className='flag' src={country.flags.svg} alt={""} style={{width:"300px"}}/>
+                <div className="info">
                 <p><span>Country: </span>{country.name.common}</p>
                 <p><span>Population: </span>{country.population}</p>
                 <p><span>Region: </span>{country.region}</p> 
-                <p><span>Capital: </span>{country.capital}</p>               
+                <p><span>Capital: </span>{country.capital}</p> 
+                </div>              
             </div>              
             )
         })}
