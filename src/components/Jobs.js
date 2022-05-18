19fcc813e89ace29a7ed48e375aa19fb66e4c921
hachi-ops <elen20@protonmail.com>
2022-05-18 16:48:58 +0100

    changes: script and styling

---
+++ src/components/Jobs.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+export default function Jobs(props) {
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+
+  React.useEffect(() => {
+    window.addEventListener("resize", function () {
+      setWindowWidth(window.innerWidth);
+    });
+  }, []);
+
+  const languages = props.listings.languages;
+  const lang = languages.map((langButton, index) => {
+    return (
+      <button
+        className="languages"
+        onClick={() => props.addFilterArr(langButton)}
+      >
+        {langButton}
+      </button>
+    );
+  });
+
+  const tools = props.listings.tools;
+  const toolsMap = tools.map((toolButton, index) => {
+    return (
+      <button className="tools" onClick={() => props.addFilterArr(toolButton)}>
+        {toolButton}
+      </button>
+    );
+  });
+
+  return (
+    <div>
+      <div className="jobPost">
+        <section className="jobInfo">
+          <img className="logo" src={props.listings.logo} alt="logo" />
+          <section className="companyDetails">
+            <section className="company">
+              <h1 className="companyName">{props.listings.company}</h1>
+              {props.listings.new && <p className="new">NEW!</p>}
+              {props.listings.featured && <p className="feature">FEATURED</p>}
+            </section>
+            <h2 className="position">{props.listings.position}</h2>
+            <p className="details">
+              {props.listings.postedAt} &#8226; {props.listings.contract}{" "}
+              &#8226; {props.listings.location}
+            </p>
+          </section>
+          {windowWidth <= 1028 && <hr></hr>}
+          <div className="buttonDiv">
+            <button
+              className="role"
+              onClick={() => props.addFilter(props.listings.role)}
+            >
+              {props.listings.role}
+            </button>
+            <button
+              className="level "
+              onClick={() => props.addFilter(props.listings.level)}
+            >
+              {props.listings.level}
+            </button>
+            {lang}
+            {toolsMap}
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
